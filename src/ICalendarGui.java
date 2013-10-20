9d9ad4c84548571589d76a8c40b4c844f343b34f
chidat <chidat@hawaii.edu>
2013-10-20 08:45:23 -1000

    added project files

---
+++ src/ICalendarGui.java
@@ -0,0 +1,270 @@
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
+import java.net.SocketException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+
+import javax.swing.*;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+
+import net.fortuna.ical4j.data.ParserException;
+import net.fortuna.ical4j.model.DateTime;
+import edu.emory.mathcs.backport.java.util.Arrays;
+ 
+public class ICalendarGui {
+    private final static String INPUT = "Input Form";
+    private final static String TEXTPANEL = "View Events";
+    private final static String OPTIONS = "Options";
+    private final static String TIMEZONELABEL = "Select time zone:";
+    private final static String DATESTARTLABEL = "  Enter start Date and time:";
+    private final static String DATEENDLABEL = "   Enter end Date and time:";
+    private final static String EVENTLABEL = "Comments:";
+    private final static String FILENAMELABEL = "                     Filename:";
+    private final static String CHECKBOXLABEL = "Weekley:";
+    private final static DateFormat dateFormat = new SimpleDateFormat("MM/dd/yyyy h:mm aa");
+    private ICalendarProject iCalP;
+    private JButton addButton;
+    private JButton cancelButton;
+    private JButton filenameSaveButton;
+    private JButton filenameLoadButton;
+    private JCheckBox weekleyCheckBox;
+    private JFormattedTextField dateStartField;
+    private JFormattedTextField dateEndField;
+    private JTextField filenameField;
+    private JTextField commentField;
+    private JTextArea calendarField;
+    private JScrollPane calendarSP;
+    private JComboBox<String> timeZones;
+    
+    public ICalendarGui(){
+    	iCalP = new ICalendarProject();
+      addButton = new JButton("Add Event");
+      cancelButton = new JButton("Cancel/Clear");
+      weekleyCheckBox = new JCheckBox("",false);
+      filenameSaveButton = new JButton("Save");
+      filenameLoadButton = new JButton("Load");
+      filenameField = new JTextField("default.ics",15);
+      dateStartField = new JFormattedTextField(dateFormat);
+      dateEndField = new JFormattedTextField(dateFormat);
+      commentField = new JTextField();
+      calendarField = new JTextArea(iCalP.getCalendar().toString());
+      calendarField.setLineWrap(true);
+      calendarField.setEditable(false);
+      calendarField.setVisible(true);
+      calendarSP = new JScrollPane(calendarField, JScrollPane.VERTICAL_SCROLLBAR_ALWAYS, JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+      calendarSP.setPreferredSize(new Dimension(350, 145));
+      //Options for the JComboBox 
+      //java's timezone ID's are not guarenteed to match iCalendars timezone ID's
+      String[] availableTimezones = java.util.TimeZone.getAvailableIDs();
+      Arrays.sort(availableTimezones);
+      timeZones = new JComboBox<String>(availableTimezones);
+    }
+    
+    private void initializeFields(){
+      java.util.Date date = new java.util.Date();
+      dateStartField.setValue(date);
+      dateEndField.setValue(date);
+      commentField.setText("");
+      dateStartField.setColumns(15);
+      dateEndField.setColumns(15);
+      commentField.setColumns(15);
+      calendarField.setText(iCalP.getCalendar().toString());
+      timeZones.setSelectedItem(java.util.TimeZone.getDefault().getID());
+    }
+    private JPanel card1(){
+    	
+    	JPanel card1 = new JPanel() {
+				private static final long serialVersionUID = 1L;
+      };
+      
+    	JLabel labelTimeZones = new JLabel(TIMEZONELABEL);
+      JLabel labelDateTimeStart = new JLabel(DATESTARTLABEL);
+      JLabel labelDateTimeEnd = new JLabel  (DATEENDLABEL);
+      JLabel labelEventComments = new JLabel(EVENTLABEL);
+      JLabel labelWeekleyCheckBox = new JLabel(CHECKBOXLABEL);
+     
+      card1.add(labelTimeZones);
+      card1.add(timeZones);
+      card1.add(labelDateTimeStart);
+      card1.add(dateStartField);
+      card1.add(labelDateTimeEnd);
+      card1.add(dateEndField);
+      card1.add(labelWeekleyCheckBox);
+      card1.add(weekleyCheckBox);
+      card1.add(labelEventComments);
+      card1.add(commentField);
+      card1.add(addButton);
+      card1.add(cancelButton); 
+      
+      timeZones.addActionListener(new ActionListener(){
+      	@Override
+      	public void actionPerformed(ActionEvent event){
+      		@SuppressWarnings("unchecked")
+					JComboBox<String> cbq = (JComboBox<String>)event.getSource();
+          String selectedTz = (String)cbq.getSelectedItem();
+      		System.out.println("Selected time zone " + selectedTz);
+      		iCalP.changeTimezone(selectedTz);
+      	}
+      });
+      
+      addButton.addActionListener(new ActionListener(){
+      	@Override
+      	public void actionPerformed(ActionEvent event){
+      		try {
+      			if(weekleyCheckBox.isSelected()){
+      				iCalP.createNewWeeklyEvent(new DateTime((java.util.Date)dateStartField.getValue()),
+      						new DateTime((java.util.Date)dateEndField.getValue()), commentField.getText());
+      			}
+      			else{
+  						iCalP.createNewEvent(new DateTime((java.util.Date)dateStartField.getValue()), 
+  								new DateTime((java.util.Date)dateEndField.getValue()), commentField.getText());
+      			}
+					} catch (SocketException e) {
+						e.printStackTrace();
+					}
+      		iCalP.writeICalToFile();
+      		JOptionPane.showMessageDialog(new JFrame(), "Added event \""+ commentField.getText()
+      				+ "\" to: " + filenameField.getText(),
+							"", JOptionPane.INFORMATION_MESSAGE);
+      		initializeFields();
+      	}
+      });
+      
+      cancelButton.addActionListener(new ActionListener(){
+      	@Override
+      	public void actionPerformed(ActionEvent event){
+      		initializeFields();
+      		
+      	}
+      });
+      
+      return card1;
+    }
+    
+    public JPanel card2(){
+    	JPanel card2 = new JPanel();
+      card2.add(calendarSP);
+      return card2;
+    }
+    
+    public JPanel card3(){
+    	JPanel card3 = new JPanel();
+    	JLabel labelFilename = new JLabel(FILENAMELABEL);
+    	
+      card3.add(labelFilename);
+      card3.add(filenameField);
+      card3.add(filenameSaveButton);
+      card3.add(filenameLoadButton);
+      
+      filenameSaveButton.addActionListener(new ActionListener(){
+      	@Override
+      	public void actionPerformed(ActionEvent event){
+      		iCalP.setFilename(filenameField.getText());
+      		iCalP.writeICalToFile();
+      	}
+      });
+      
+      filenameLoadButton.addActionListener(new ActionListener(){
+      	@Override
+      	public void actionPerformed(ActionEvent event){
+      		loadICal();
+      	}
+      });
+      return card3;
+    }
+    
+    public void addComponentToPane(Container pane) {
+        JTabbedPane tabbedPane = new JTabbedPane();
+        
+        initializeFields();
+        JPanel card1 = card1();
+        JPanel card2 = card2();
+        JPanel card3 = card3();
+        
+        tabbedPane.addChangeListener(new ChangeListener() {
+          @Override
+          public void stateChanged(ChangeEvent e) {
+              if (e.getSource() instanceof JTabbedPane) {
+                  JTabbedPane pane = (JTabbedPane) e.getSource();
+                  if(pane.getSelectedIndex() == 1){
+                  	calendarField.setText(iCalP.getCalendar().toString());
+                  }
+              }
+          }
+        });
+        
+        tabbedPane.addTab(INPUT, card1);
+        tabbedPane.addTab(TEXTPANEL, card2);
+        tabbedPane.addTab(OPTIONS, card3);
+        pane.add(tabbedPane, BorderLayout.CENTER);
+    }
+    
+    private void loadICal(){
+  			try {
+					iCalP.loadCalendar(filenameField.getText());
+					filenameField.setText(iCalP.getFilename());
+      		JOptionPane.showMessageDialog(new JFrame(), "Sucessfully loaded file: " + filenameField.getText(),
+							"Load Complete", JOptionPane.INFORMATION_MESSAGE);
+				} catch (IOException e) {
+					JOptionPane.showMessageDialog(new JFrame(), "I/O Exception! Failed to load file: " + filenameField.getText(),
+							"ERROR", JOptionPane.INFORMATION_MESSAGE);
+					e.printStackTrace();
+				} catch (ParserException e) {
+					JOptionPane.showMessageDialog(new JFrame(), "Parser Exception! Failed to load file: " + filenameField.getText(),
+							"ERROR", JOptionPane.INFORMATION_MESSAGE);
+					e.printStackTrace();
+				}
+    }
+ 
+    /**
+     * Create the GUI and show it.  For thread safety,
+     * this method should be invoked from the
+     * event dispatch thread.
+     */
+    private static void createAndShowGUI() {
+        //Create and set up the window.
+        JFrame frame = new JFrame("Flaglers iCalendar App");
+        frame.setAlwaysOnTop(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(380,220);
+        //Create and set up the content pane.
+        ICalendarGui gui = new ICalendarGui();
+        gui.addComponentToPane(frame.getContentPane());
+ 
+        //Display the window.
+        //frame.pack();
+        frame.setVisible(true);
+    }
+ 
+    public static void main(String[] args) {
+    	@SuppressWarnings("unused")
+    	ICalendarGui run = new ICalendarGui();
+    	/* Use an appropriate Look and Feel */
+      try {
+          //UIManager.setLookAndFeel("com.sun.java.swing.plaf.windows.WindowsLookAndFeel");
+          UIManager.setLookAndFeel("javax.swing.plaf.metal.MetalLookAndFeel");
+      } catch (UnsupportedLookAndFeelException ex) {
+          ex.printStackTrace();
+      } catch (IllegalAccessException ex) {
+          ex.printStackTrace();
+      } catch (InstantiationException ex) {
+          ex.printStackTrace();
+      } catch (ClassNotFoundException ex) {
+          ex.printStackTrace();
+      }
+      /* Turn off metal's use of bold fonts */
+      UIManager.put("swing.boldMetal", Boolean.FALSE);
+       
+      //Schedule a job for the event dispatch thread:
+      //creating and showing this application's GUI.
+      javax.swing.SwingUtilities.invokeLater(new Runnable() {
+          @Override
+					public void run() {
+              createAndShowGUI();
+          }
+      });
+    }
+}
---
+++ src/ICalendarProject.java
@@ -0,0 +1,195 @@
+/* ICalendarExample.java */
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.net.SocketException;
+import java.util.Iterator;
+
+import net.fortuna.ical4j.data.CalendarBuilder;
+import net.fortuna.ical4j.data.CalendarOutputter;
+import net.fortuna.ical4j.data.ParserException;
+import net.fortuna.ical4j.model.Calendar;
+import net.fortuna.ical4j.model.Component;
+import net.fortuna.ical4j.model.Date;
+import net.fortuna.ical4j.model.DateTime;
+import net.fortuna.ical4j.model.Property;
+import net.fortuna.ical4j.model.Recur;
+import net.fortuna.ical4j.model.TimeZone;
+import net.fortuna.ical4j.model.TimeZoneRegistry;
+import net.fortuna.ical4j.model.TimeZoneRegistryFactory;
+import net.fortuna.ical4j.model.ValidationException;
+import net.fortuna.ical4j.model.component.VEvent;
+import net.fortuna.ical4j.model.component.VTimeZone;
+import net.fortuna.ical4j.model.parameter.Value;
+import net.fortuna.ical4j.model.property.CalScale;
+import net.fortuna.ical4j.model.property.ProdId;
+import net.fortuna.ical4j.model.property.RRule;
+import net.fortuna.ical4j.model.property.Version;
+import net.fortuna.ical4j.util.UidGenerator;
+
+public class ICalendarProject {
+
+	private Calendar calendar;
+	private TimeZone timezone;
+	private VTimeZone tz;
+	private TimeZoneRegistry registry;
+	private String filename = "default.ics";
+ 
+	public static void main(String[] args) throws IOException, ParserException{
+		//ICalendarProject ical = new ICalendarProject();
+	}
+	
+	//Creating a new calendar
+	public ICalendarProject(){ 
+		calendar = new Calendar();
+		registry = TimeZoneRegistryFactory.getInstance().createRegistry();
+		timezone = registry.getTimeZone(TimeZone.getDefault().getID());
+		tz = timezone.getVTimeZone();
+		calendar.getProperties().add(new ProdId("Team Flagler's Super iCal4j Calender App Version 6.9"));
+		calendar.getProperties().add(Version.VERSION_2_0);
+		calendar.getProperties().add(CalScale.GREGORIAN);		
+		System.out.println(tz.toString());
+	}
+	
+//Creating a new calendar
+	public ICalendarProject(String selectedTimezone){ 
+		calendar = new Calendar();
+		registry = TimeZoneRegistryFactory.getInstance().createRegistry();
+		timezone = registry.getTimeZone(selectedTimezone);
+		tz = timezone.getVTimeZone();
+		calendar.getProperties().add(new ProdId("Team Flagler's Super iCal4j Calender App Version 6.9"));
+		calendar.getProperties().add(Version.VERSION_2_0);
+		calendar.getProperties().add(CalScale.GREGORIAN);		
+	}
+	
+	public void loadCalendar(String file) throws IOException, ParserException{
+		FileReader fR = new FileReader(file);
+		BufferedReader bR = new BufferedReader(fR);
+		CalendarBuilder cBuilder = new CalendarBuilder();        	
+		setCalendar(cBuilder.build(bR));
+		setFilename(file);
+  }
+	
+	public String getFilename(){
+		return filename;
+	}
+	
+	public void setFilename(String name){
+		filename = name;
+	}
+	
+	public Calendar getCalendar(){
+		return calendar;
+	}
+	
+	public void setCalendar(Calendar cal){
+		calendar = cal;
+	}
+	
+	public TimeZone getTimeZone(){
+		return timezone;
+	}
+	
+	public void changeTimezone(String selectedTimezone){
+		registry = TimeZoneRegistryFactory.getInstance().createRegistry();
+		timezone = registry.getTimeZone(selectedTimezone);
+		tz = timezone.getVTimeZone();
+	}
+
+	//Creating an event
+	//months start at 0 (January)
+	public void createNewEvent(Date eventStart, Date eventEnd,  String eventDescription) throws SocketException{
+	  
+	  //creating event
+	  VEvent iCalEvent = new VEvent(eventStart, eventEnd, eventDescription);
+	  //adding timezone info
+	  iCalEvent.getProperties().add(tz.getTimeZoneId());
+		UidGenerator uidGenerator = new UidGenerator("1");
+		iCalEvent.getProperties().add(uidGenerator.generateUid());
+		calendar.getComponents().add(iCalEvent);
+
+	}
+	
+//Creating an all day event
+	//months start at 0 (January)
+	public void createNewAllDayEvent(Date eventDay, String eventDescription) throws SocketException{
+	  
+		
+	  //creating event
+	  VEvent iCalEvent = new VEvent(eventDay, eventDescription);
+	  Recur recur = new Recur(Recur.WEEKLY,null);
+	  recur.setUntil( new DateTime(eventDay.getTime()) );
+
+	  RRule rule = new RRule(recur);
+	  iCalEvent.getProperties().add(rule);
+	  // initialise as an all-day event..
+	  iCalEvent.getProperties().getProperty(Property.DTSTART).getParameters().add(Value.DATE);
+		  
+		UidGenerator uidGenerator = new UidGenerator("1");
+		iCalEvent.getProperties().add(uidGenerator.generateUid());
+		calendar.getComponents().add(iCalEvent);
+
+	}
+	
+	public void createNewWeeklyEvent(Date eventDay, Date RepeatUntil, String eventDescription) throws SocketException{
+		
+	  //creating event
+	  VEvent iCalEvent = new VEvent(eventDay, eventDescription);
+	  Recur recur = new Recur(Recur.WEEKLY,null);
+	  recur.setUntil( new DateTime(RepeatUntil.getTime()) );
+
+	  RRule rule = new RRule(recur);
+	  iCalEvent.getProperties().add(rule);
+	  // initialise as an all-day event..
+	  iCalEvent.getProperties().getProperty(Property.DTSTART).getParameters().add(Value.DATE);
+		  
+		UidGenerator uidGenerator = new UidGenerator("1");
+		iCalEvent.getProperties().add(uidGenerator.generateUid());
+		calendar.getComponents().add(iCalEvent);
+
+	}
+	
+	@SuppressWarnings("rawtypes")
+	public String parseICalendar() throws IOException, ParserException{
+	//Now Parsing an iCalendar file
+	  FileInputStream fin = new FileInputStream(filename);
+	  String str = new String();
+	  CalendarBuilder builder = new CalendarBuilder();
+	  calendar = builder.build(fin);
+	  //Iterating over a Calendar
+	  for (Iterator i = calendar.getComponents().iterator(); i.hasNext();) {
+	      Component component = (Component) i.next();
+	      System.out.println("Component [" + component.getName() + "]");
+
+	      for (Iterator j = component.getProperties().iterator(); j.hasNext();) {
+	          Property property = (Property) j.next();
+	          str = str + "Property [" + property.getName() + ", " + property.getValue() + "]" + "/n";
+	          System.out.println(str);
+	      }
+	  }
+	  
+	  return str;
+	}
+	
+	//Saving an iCalendar file
+	public void writeICalToFile(){
+		try{
+		  FileOutputStream fout = new FileOutputStream(filename);
+		  CalendarOutputter outputter = new CalendarOutputter();
+		  outputter.setValidating(false);
+		  outputter.output(calendar, fout);
+		}catch(FileNotFoundException e){
+			System.out.println("File does not exist in the correct directory.");
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ValidationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
