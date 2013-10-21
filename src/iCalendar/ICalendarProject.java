6c7d3ea98cb26a7227f57c7d20ab7cc692c8f9fa
chidat <chidat@hawaii.edu>
2013-10-20 22:19:10 -1000

    Added boolean return values to methods. Added more test cases.

--- src/iCalendar/ICalendarProject.java
+++ src/iCalendar/ICalendarProject.java
@@ -1,4 +1,5 @@
 package iCalendar;
+
 /* ICalendarExample.java */
 
 import java.io.BufferedReader;
@@ -9,7 +10,6 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.net.SocketException;
 import java.util.Iterator;
-
 import net.fortuna.ical4j.data.CalendarBuilder;
 import net.fortuna.ical4j.data.CalendarOutputter;
 import net.fortuna.ical4j.data.ParserException;
@@ -34,163 +34,187 @@ import net.fortuna.ical4j.util.UidGenerator;
 
 public class ICalendarProject {
 
-	private Calendar calendar;
-	private TimeZone timezone;
-	private VTimeZone tz;
-	private TimeZoneRegistry registry;
-	private String filename = "default.ics";
- 
-	public static void main(String[] args) throws IOException, ParserException{
-		//ICalendarProject ical = new ICalendarProject();
-	}
-	
-	//Creating a new calendar
-	public ICalendarProject(){ 
-		calendar = new Calendar();
-		registry = TimeZoneRegistryFactory.getInstance().createRegistry();
-		timezone = registry.getTimeZone(TimeZone.getDefault().getID());
-		tz = timezone.getVTimeZone();
-		calendar.getProperties().add(new ProdId("Team Flagler's Super iCal4j Calender App Version 6.9"));
-		calendar.getProperties().add(Version.VERSION_2_0);
-		calendar.getProperties().add(CalScale.GREGORIAN);		
-		System.out.println(tz.toString());
-	}
-	
-//Creating a new calendar
-	public ICalendarProject(String selectedTimezone){ 
-		calendar = new Calendar();
-		registry = TimeZoneRegistryFactory.getInstance().createRegistry();
-		timezone = registry.getTimeZone(selectedTimezone);
-		tz = timezone.getVTimeZone();
-		calendar.getProperties().add(new ProdId("Team Flagler's Super iCal4j Calender App Version 6.9"));
-		calendar.getProperties().add(Version.VERSION_2_0);
-		calendar.getProperties().add(CalScale.GREGORIAN);		
-	}
-	
-	public void loadCalendar(String file) throws IOException, ParserException{
-		FileReader fR = new FileReader(file);
-		BufferedReader bR = new BufferedReader(fR);
-		CalendarBuilder cBuilder = new CalendarBuilder();        	
-		setCalendar(cBuilder.build(bR));
-		setFilename(file);
+  private Calendar calendar;
+  private TimeZone timezone;
+  private VTimeZone tz;
+  private TimeZoneRegistry registry;
+  private String filename = "default.ics";
+
+  public static void main(String[] args) throws IOException, ParserException {
+    // ICalendarProject ical = new ICalendarProject();
   }
-	
-	public String getFilename(){
-		return filename;
-	}
-	
-	public void setFilename(String name){
-		filename = name;
-	}
-	
-	public Calendar getCalendar(){
-		return calendar;
-	}
-	
-	public void setCalendar(Calendar cal){
-		calendar = cal;
-	}
-	
-	public TimeZone getTimeZone(){
-		return timezone;
-	}
-	
-	public void changeTimezone(String selectedTimezone){
-		registry = TimeZoneRegistryFactory.getInstance().createRegistry();
-		timezone = registry.getTimeZone(selectedTimezone);
-		tz = timezone.getVTimeZone();
-	}
 
-	//Creating an event
-	//months start at 0 (January)
-	public void createNewEvent(Date eventStart, Date eventEnd,  String eventDescription) throws SocketException{
-	  
-	  //creating event
-	  VEvent iCalEvent = new VEvent(eventStart, eventEnd, eventDescription);
-	  //adding timezone info
-	  iCalEvent.getProperties().add(tz.getTimeZoneId());
-		UidGenerator uidGenerator = new UidGenerator("1");
-		iCalEvent.getProperties().add(uidGenerator.generateUid());
-		calendar.getComponents().add(iCalEvent);
+  // Creating a new calendar
+  public ICalendarProject() {
+    calendar = new Calendar();
+    registry = TimeZoneRegistryFactory.getInstance().createRegistry();
+    timezone = registry.getTimeZone(TimeZone.getDefault().getID());
+    tz = timezone.getVTimeZone();
+    calendar.getProperties()
+        .add(new ProdId("Team Flagler's Super iCal4j Calender App Version 6.9"));
+    calendar.getProperties().add(Version.VERSION_2_0);
+    calendar.getProperties().add(CalScale.GREGORIAN);
+    System.out.println(tz.toString());
+  }
 
-	}
-	
-//Creating an all day event
-	//months start at 0 (January)
-	public void createNewAllDayEvent(Date eventDay, String eventDescription) throws SocketException{
-	  
-		
-	  //creating event
-	  VEvent iCalEvent = new VEvent(eventDay, eventDescription);
-	  Recur recur = new Recur(Recur.WEEKLY,null);
-	  recur.setUntil( new DateTime(eventDay.getTime()) );
+  // Creating a new calendar
+  public ICalendarProject(String selectedTimezone) {
+    calendar = new Calendar();
+    registry = TimeZoneRegistryFactory.getInstance().createRegistry();
+    timezone = registry.getTimeZone(selectedTimezone);
+    tz = timezone.getVTimeZone();
+    calendar.getProperties()
+        .add(new ProdId("Team Flagler's Super iCal4j Calender App Version 6.9"));
+    calendar.getProperties().add(Version.VERSION_2_0);
+    calendar.getProperties().add(CalScale.GREGORIAN);
+  }
 
-	  RRule rule = new RRule(recur);
-	  iCalEvent.getProperties().add(rule);
-	  // initialise as an all-day event..
-	  iCalEvent.getProperties().getProperty(Property.DTSTART).getParameters().add(Value.DATE);
-		  
-		UidGenerator uidGenerator = new UidGenerator("1");
-		iCalEvent.getProperties().add(uidGenerator.generateUid());
-		calendar.getComponents().add(iCalEvent);
+  public void loadCalendar(String file) throws IOException, ParserException {
+    FileReader fR = new FileReader(file);
+    BufferedReader bR = new BufferedReader(fR);
+    CalendarBuilder cBuilder = new CalendarBuilder();
+    setCalendar(cBuilder.build(bR));
+    setFilename(file);
+  }
 
-	}
-	
-	public void createNewWeeklyEvent(Date eventDay, Date RepeatUntil, String eventDescription) throws SocketException{
-		
-	  //creating event
-	  VEvent iCalEvent = new VEvent(eventDay, eventDescription);
-	  Recur recur = new Recur(Recur.WEEKLY,null);
-	  recur.setUntil( new DateTime(RepeatUntil.getTime()) );
+  public String getFilename() {
+    return filename;
+  }
 
-	  RRule rule = new RRule(recur);
-	  iCalEvent.getProperties().add(rule);
-	  // initialise as an all-day event..
-	  iCalEvent.getProperties().getProperty(Property.DTSTART).getParameters().add(Value.DATE);
-		  
-		UidGenerator uidGenerator = new UidGenerator("1");
-		iCalEvent.getProperties().add(uidGenerator.generateUid());
-		calendar.getComponents().add(iCalEvent);
+  public void setFilename(String name) {
+    filename = name;
+  }
 
-	}
-	
-	@SuppressWarnings("rawtypes")
-	public String parseICalendar() throws IOException, ParserException{
-	//Now Parsing an iCalendar file
-	  FileInputStream fin = new FileInputStream(filename);
-	  String str = new String();
-	  CalendarBuilder builder = new CalendarBuilder();
-	  calendar = builder.build(fin);
-	  //Iterating over a Calendar
-	  for (Iterator i = calendar.getComponents().iterator(); i.hasNext();) {
-	      Component component = (Component) i.next();
-	      System.out.println("Component [" + component.getName() + "]");
+  public Calendar getCalendar() {
+    return calendar;
+  }
 
-	      for (Iterator j = component.getProperties().iterator(); j.hasNext();) {
-	          Property property = (Property) j.next();
-	          str = str + "Property [" + property.getName() + ", " + property.getValue() + "]" + "/n";
-	          System.out.println(str);
-	      }
-	  }
-	  
-	  return str;
-	}
-	
-	//Saving an iCalendar file
-	public void writeICalToFile(){
-		try{
-		  FileOutputStream fout = new FileOutputStream(filename);
-		  CalendarOutputter outputter = new CalendarOutputter();
-		  outputter.setValidating(false);
-		  outputter.output(calendar, fout);
-		}catch(FileNotFoundException e){
-			System.out.println("File does not exist in the correct directory.");
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (ValidationException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
+  public void setCalendar(Calendar cal) {
+    calendar = cal;
+  }
+
+  public TimeZone getTimeZone() {
+    return timezone;
+  }
+
+  public void changeTimezone(String selectedTimezone) {
+    registry = TimeZoneRegistryFactory.getInstance().createRegistry();
+    timezone = registry.getTimeZone(selectedTimezone);
+    tz = timezone.getVTimeZone();
+  }
+
+  // Creating an event
+  // months start at 0 (January)
+  public boolean createNewEvent(Date eventStart, Date eventEnd, String eventDescription) throws SocketException {
+
+    boolean returnValue = false;
+
+    // creating event
+    VEvent iCalEvent = new VEvent(eventStart, eventEnd, eventDescription);
+    // adding timezone info
+    iCalEvent.getProperties().add(tz.getTimeZoneId());
+    UidGenerator uidGenerator = new UidGenerator("1");
+    iCalEvent.getProperties().add(uidGenerator.generateUid());
+    calendar.getComponents().add(iCalEvent);
+    returnValue = true;
+
+    return returnValue;
+
+  }
+
+  // Creating an all day event
+  // months start at 0 (January)
+  public boolean createNewAllDayEvent(Date eventDay, String eventDescription) throws SocketException {
+    
+    boolean returnValue = false;
+
+    // creating event
+    VEvent iCalEvent = new VEvent(eventDay, eventDescription);
+    Recur recur = new Recur(Recur.WEEKLY, null);
+    recur.setUntil(new DateTime(eventDay.getTime()));
+
+    RRule rule = new RRule(recur);
+    iCalEvent.getProperties().add(rule);
+    // initialize as an all-day event..
+    iCalEvent.getProperties().getProperty(Property.DTSTART).getParameters().add(Value.DATE);
+
+    UidGenerator uidGenerator = new UidGenerator("1");
+    iCalEvent.getProperties().add(uidGenerator.generateUid());
+    calendar.getComponents().add(iCalEvent);
+    returnValue = true;
+    
+    return returnValue;
+
+  }
+
+  public boolean createNewWeeklyEvent(Date eventDay, Date RepeatUntil, String eventDescription) throws SocketException {
+
+    boolean returnValue = false;
+    
+    // creating event
+    VEvent iCalEvent = new VEvent(eventDay, eventDescription);
+    Recur recur = new Recur(Recur.WEEKLY, null);
+    recur.setUntil(new DateTime(RepeatUntil.getTime()));
+
+    RRule rule = new RRule(recur);
+    iCalEvent.getProperties().add(rule);
+    // initialize as an all-day event..
+    iCalEvent.getProperties().getProperty(Property.DTSTART).getParameters().add(Value.DATE);
+
+    UidGenerator uidGenerator = new UidGenerator("1");
+    iCalEvent.getProperties().add(uidGenerator.generateUid());
+    calendar.getComponents().add(iCalEvent);
+    returnValue = true;
+
+    return returnValue;
+
+  }
+
+  @SuppressWarnings("rawtypes")
+  public String parseICalendar() throws IOException, ParserException {
+    // Now Parsing an iCalendar file
+    FileInputStream fin = new FileInputStream(filename);
+    String str = new String();
+    CalendarBuilder builder = new CalendarBuilder();
+    calendar = builder.build(fin);
+    // Iterating over a Calendar
+    for (Iterator i = calendar.getComponents().iterator(); i.hasNext();) {
+      Component component = (Component) i.next();
+      System.out.println("Component [" + component.getName() + "]");
+
+      for (Iterator j = component.getProperties().iterator(); j.hasNext();) {
+        Property property = (Property) j.next();
+        str = str + "Property [" + property.getName() + ", " + property.getValue() + "]" + "/n";
+        System.out.println(str);
+      }
+    }
+
+    return str;
+  }
+
+  // Saving an iCalendar file
+  public boolean writeICalToFile() {
+
+    boolean returnValue = false;
+
+    try {
+      FileOutputStream fout = new FileOutputStream(filename);
+      CalendarOutputter outputter = new CalendarOutputter();
+      outputter.setValidating(false);
+      outputter.output(calendar, fout);
+      returnValue = true;
+    }
+    catch (FileNotFoundException e) {
+      System.out.println("File does not exist in the correct directory.");
+    }
+    catch (IOException e) {
+      e.printStackTrace();
+    }
+    catch (ValidationException e) {
+      e.printStackTrace();
+    }
+
+    return returnValue;
+
+  }
 }
--- src/test/ICalendarProjectTest.java
+++ src/test/ICalendarProjectTest.java
@@ -1,18 +1,28 @@
 package test;
 
 import static org.junit.Assert.*;
+import java.io.IOException;
+import java.net.SocketException;
+import java.text.ParseException;
+import javax.swing.JTextField;
 import iCalendar.ICalendarProject;
+import net.fortuna.ical4j.data.ParserException;
+import net.fortuna.ical4j.model.Calendar;
+import net.fortuna.ical4j.model.Date;
 import org.junit.Test;
 
 public class ICalendarProjectTest {
   
-  private ICalendarProject iCalProject = new ICalendarProject();
+  ICalendarProject iCalProject = new ICalendarProject();
 
   @Test
   public void testLoadCalendar() {
     fail("Not yet implemented");
   }
 
+  /**
+   * Tests the getFilename() method.
+   */
   @Test
   public void testGetFilename() {
     assertEquals("Check file name", "default.ics", iCalProject.getFilename());
@@ -23,9 +33,13 @@ public class ICalendarProjectTest {
     fail("Not yet implemented");
   }
 
+  /**
+   * Tests the getCalendar() method.
+   */
   @Test
   public void testGetCalendar() {
-    fail("Not yet implemented");
+    Calendar calendar = new Calendar();
+    assertEquals("Check calendar", calendar, iCalProject.getCalendar());
   }
 
   @Test
@@ -43,29 +57,47 @@ public class ICalendarProjectTest {
     fail("Not yet implemented");
   }
 
+  /**
+   * Tests the createNewEvent() method.
+   * @throws SocketException
+   * @throws ParseException
+   */
   @Test
-  public void testCreateNewEvent() {
-    fail("Not yet implemented");
+  public void testCreateNewEvent() throws SocketException, ParseException {
+    assertTrue("New event not created.", iCalProject.createNewEvent(new Date("20131020"), new Date("20131021"), new JTextField().getText()));
+  }
+
+  /**
+   * Tests the createNewAllDayEvent() method.
+   * @throws SocketException
+   * @throws ParseException
+   */
+  @Test
+  public void testCreateNewAllDayEvent() throws SocketException, ParseException {
+    assertTrue("New all-day event not created.", iCalProject.createNewAllDayEvent(new Date("20131020"), "All Day Event 1"));
   }
 
   @Test
-  public void testCreateNewAllDayEvent() {
-    fail("Not yet implemented");
+  public void testCreateNewWeeklyEvent() throws SocketException, ParseException {
+    assertTrue("New weekly event not created.", iCalProject.createNewWeeklyEvent(new Date("20131020"), new Date("20131120"), "Weekly Event 1"));
   }
 
+  /**
+   * Tests the parseICalendar() method.
+   * @throws IOException
+   * @throws ParserException
+   */
   @Test
-  public void testCreateNewWeeklyEvent() {
-    fail("Not yet implemented");
+  public void testParseICalendar() throws IOException, ParserException {
+    assertEquals("iCalendar not parsed correctly.", "", iCalProject.parseICalendar());
   }
 
-  @Test
-  public void testParseICalendar() {
-    fail("Not yet implemented");
-  }
-
+  /**
+   * Tests the writeICalToFile() method.
+   */
   @Test
   public void testWriteICalToFile() {
-    fail("Not yet implemented");
+    assertTrue("Calendar not output.", iCalProject.writeICalToFile());
   }
 
 }
